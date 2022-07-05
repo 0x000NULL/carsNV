d8282b9e0bf440859075489ab0d960e37f11bbaa
ethanaldrich <ethanaldrich9986@gmail.com>
2022-07-05 15:19:06 +0000

    running

---
+++ db/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO `administrator` (`adminId`, `username`, `password`, `email`, `admin_level`, `last_update`)
+VALUES (4, 'ethan', 'Painkiller69!', 'ethanaldrich9986@gmail.com', '3', '2019-05-10 02:44:41');
